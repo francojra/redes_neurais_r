30229e4156c8b077186cac3d81222fc35f7e9f7f
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-09-26 23:18:47 -0300

    Add files via upload

--- redes_neurais.R
+++ redes_neurais.R
@@ -4,5 +4,13 @@
 # Data: 26/09/23 ---------------------------------------------------------------------------------------------------------------------------
 # Referência: https://rpubs.com/mbatista/545937 --------------------------------------------------------------------------------------------
 
+# Introdução -------------------------------------------------------------------------------------------------------------------------------
+
+### Segundo a concepção de Steven Pinker, no livro “Como a mente funciona”, “Pensar é 
+### computação”. Essa tentativa de criarmos redes neurais que imitam o funcionamento do 
+### cérebro humano sempre me fascinou. Acredito que fascina qualquer pessoa quando 
+### escuta falar sobre inteligência artificial.
+
+### Será utilizado dois pacotes do CRAN para desenvolver essa introdução a redes neurais.
 
 
